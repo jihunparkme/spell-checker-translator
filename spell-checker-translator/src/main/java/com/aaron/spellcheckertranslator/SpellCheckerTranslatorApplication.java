a0aaad2b8d1e8a0fdda3e6247d6e1933b45f905e
Jihun Park <jihunpark.tech@gmail.com>
2022-12-12 00:27:38 +0900

    init project
    Signed-off-by: Jihun Park <jihunpark.tech@gmail.com>

---
+++ spell-checker-translator/src/main/java/com/aaron/spellcheckertranslator/SpellCheckerTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.aaron.spellcheckertranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellCheckerTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellCheckerTranslatorApplication.class, args);
+	}
+
+}
