026dd2d03968751c819068651dabbca404ac08dc
Jihun Park <jihunpark.tech@gmail.com>
2022-12-16 00:57:16 +0900

    test: edit response return type

--- spell-checker-translator/src/test/java/com/aaron/spellcheckertranslator/spellchecker/service/PusanSpellCheckerServiceTest.java
+++ spell-checker-translator/src/test/java/com/aaron/spellcheckertranslator/spellchecker/service/PusanSpellCheckerServiceTest.java
@@ -1,5 +1,6 @@
 package com.aaron.spellcheckertranslator.spellchecker.service;
 
+import com.aaron.spellcheckertranslator.spellchecker.domain.SpellCheckerResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,38 +19,31 @@ class PusanSpellCheckerServiceTest {
 
     @Test
     void spellCheck1() {
-        String result = spellCheckerService.spellCheck("아빠가방에들어가십니다.");
-        assertThat(result).isEqualTo("아빠가 방에 들어가십니다.");
+        SpellCheckerResponse spellCheckerResponse = spellCheckerService.spellCheck("아빠가방에들어가십니다.");
+        assertThat(spellCheckerResponse.getCorrectedText()).isEqualTo("아빠가 방에 들어가십니다.");
     }
 
     @Test
     void spellCheck2() {
-        String result = spellCheckerService.spellCheck("오후부터는 눈이 시작되면서 퇴근길 시민들도 빙판길이나 차량 정체를 우려해 일찍부터 발길을 서두르는 모습이었습니다.");
-        assertThat(result).isEqualTo("오후부터는 눈이 시작되면서 퇴근길 시민들도 빙판길이나 차량 정체를 우려해 일찍부터 발길을 서두르는 모습이었습니다.");
+        SpellCheckerResponse spellCheckerResponse = spellCheckerService.spellCheck("오후부터는 눈이 시작되면서 퇴근길 시민들도 빙판길이나 차량 정체를 우려해 일찍부터 발길을 서두르는 모습이었습니다.");
+        assertThat(spellCheckerResponse.getCorrectedText()).isEqualTo("오후부터는 눈이 시작되면서 퇴근길 시민들도 빙판길이나 차량 정체를 우려해 일찍부터 발길을 서두르는 모습이었습니다.");
     }
 
     @Test
     void spellCheck3() {
-        String result = spellCheckerService.spellCheck("내이름은아론입니다");
-        assertThat(result).isEqualTo("내 이름은 아론입니다");
+        SpellCheckerResponse spellCheckerResponse = spellCheckerService.spellCheck("내이름은아론입니다");
+        assertThat(spellCheckerResponse.getCorrectedText()).isEqualTo("내 이름은 아론입니다");
     }
 
     @Test
     void spellCheck4() {
-        String result = spellCheckerService.spellCheck("내 이름은 아론입니다...");
-        assertThat(result).isEqualTo("내 이름은 아론입니다...");
+        SpellCheckerResponse spellCheckerResponse = spellCheckerService.spellCheck("내 이름은 아론입니다...");
+        assertThat(spellCheckerResponse.getCorrectedText()).isEqualTo("내 이름은 아론입니다...");
     }
 
     @Test
     void spellCheck5() {
-        String result = spellCheckerService.spellCheck("대상 클래스만 다를뿐 로직은 유사하고, 대상 클래스 개수만큼 프록시 클래스 생성 필요");
-        assertThat(result).isEqualTo("대상 클래스만 다를 뿐 로직은 유사하고, 대상 클래스 개수만큼 프락시 클래스 생성 필요");
-    }
-
-    @Test
-    void string_replace_test() {
-        String orgStr = "대상 클래스만 다를뿐 로직은 유사하고, 대상 클래스 개수만큼 프록시 클래스 생성 필요";
-        String result = orgStr.replace("다를뿐", "다를 뿐");
-        assertThat(result).isEqualTo("대상 클래스만 다를 뿐 로직은 유사하고, 대상 클래스 개수만큼 프록시 클래스 생성 필요");
+        SpellCheckerResponse spellCheckerResponse = spellCheckerService.spellCheck("대상 클래스만 다를뿐 로직은 유사하고, 대상 클래스 개수만큼 프록시 클래스 생성 필요");
+        assertThat(spellCheckerResponse.getCorrectedText()).isEqualTo("대상 클래스만 다를 뿐 로직은 유사하고, 대상 클래스 개수만큼 프락시 클래스 생성 필요");
     }
 }
