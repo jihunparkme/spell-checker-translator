a285ed9b4d3c75d354c357bcc9235254572efa5a
Jihun Park <jihunpark.tech@gmail.com>
2022-12-28 23:17:12 +0900

    feat: 원문 노출 영역 추가

--- spell-checker-translator/src/main/resources/static/js/index-fn.js
+++ spell-checker-translator/src/main/resources/static/js/index-fn.js
@@ -7,6 +7,8 @@ function fnSend() {
     }).done(function(result) {
         console.log(result);
         $('#output').val(result.translatedText);
+        $('#corrected-original-text').val(result.originalText);
+        $('#translated-original-text').val(result.originalText);
     }).fail(function (error) {
         $('#output').val("에러가 발생하였습니다. 잠시 후 다시 시도해 주세요.");
     });
