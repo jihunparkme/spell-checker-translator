3898afb5d86e201a3bed84cc3d6b97edae3638dc
Jihun Park <jihunpark.tech@gmail.com>
2022-12-29 18:53:08 +0900

    fix: test code

--- spell-checker-translator/src/test/java/com/aaron/spellcheckertranslator/translator/papago/service/PapagoTransServiceTest.java
+++ spell-checker-translator/src/test/java/com/aaron/spellcheckertranslator/translator/papago/service/PapagoTransServiceTest.java
@@ -1,5 +1,6 @@
 package com.aaron.spellcheckertranslator.translator.papago.service;
 
+import com.aaron.spellcheckertranslator.translator.common.domain.TranslatorRequest;
 import com.aaron.spellcheckertranslator.translator.google.domain.Language;
 import com.aaron.spellcheckertranslator.translator.common.domain.TranslatorResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,12 @@ class PapagoTransServiceTest {
     @Test
     void translate01() throws Exception {
         String text = "문화예술로서 게임의 역할과 가능성에 대한 의견도 내놨다. 당장은 실용적으로 보이지 않을지 몰라도 아름다움을 통하거나 기대 이상의 집요한 노력 또는 새로운 기술과 시도가 주는 충격적인 경험을 만들어 낸다는 점에서 예술과 게임은 매우 많이 닮아 있다는 견해다.";
-        TranslatorResponse translate = transService.translate(text, Language.KOREAN.getLang(), Language.ENGLISH.getLang());
+        TranslatorResponse translate = transService.translate(
+                TranslatorRequest.builder()
+                        .text(text)
+                        .srcLang(Language.KOREAN.getLang())
+                        .tgtLang(Language.ENGLISH.getLang())
+                        .build());
         assertThat(translate.getTranslatedText()).isEqualTo("He also expressed opinions on the role and possibility of games as culture and art. Although it may not seem practical right now, art and games are very similar in that they create a shocking experience of persistent efforts or new technologies and attempts through beauty or beyond expectations.");
     }
 }
