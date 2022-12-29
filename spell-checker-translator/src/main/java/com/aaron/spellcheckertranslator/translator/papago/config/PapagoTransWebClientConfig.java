8a99d44bcb58eaabb9c717eff76975bdbf74279f
Jihun Park <jihunpark.tech@gmail.com>
2022-12-29 17:50:46 +0900

    add: papago api web client config

---
+++ spell-checker-translator/src/main/java/com/aaron/spellcheckertranslator/translator/papago/config/PapagoTransWebClientConfig.java
@@ -0,0 +1,42 @@
+package com.aaron.spellcheckertranslator.translator.papago.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class PapagoTransWebClientConfig {
+
+    @Value("${translator.papago.url}")
+    private String apiUrl;
+
+    @Value("${translator.papago.client-id}")
+    private String clientId;
+
+    @Value("${translator.papago.client-secret}")
+    private String clientSecret;
+
+    public HttpURLConnection createHttpClient() {
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    public Map<String, String> getRequestHeaders() {
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        return requestHeaders;
+    }
+}
