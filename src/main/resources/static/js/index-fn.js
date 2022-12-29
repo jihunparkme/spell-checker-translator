5fbb4443304ef2f4d4907b06ed985db32fd665e8
Jihun Park <jihunpark.tech@gmail.com>
2022-12-29 21:17:41 +0900

    feat: apply papago translator api

--- src/main/resources/static/js/index-fn.js
+++ src/main/resources/static/js/index-fn.js
@@ -1,13 +1,14 @@
 function fnSend() {
     $.ajax({
         type: 'POST',
-        url: "/spl-ch-trnsl/request",
+        url: "/spl-ch-trnsl/request-tr-pp",
         dataType: 'json',
         data: $("#form").serialize(),
     }).done(function(result) {
         console.log(result);
         $('#output').val(result.translatedText);
-        $('#corrected-original-text').val(result.originalText);
+        $('#original-text').val(result.originalText);
+        $('#corrected-original-text').val(result.correctedText);
     }).fail(function (error) {
         $('#output').val("에러가 발생하였습니다. 잠시 후 다시 시도해 주세요.");
     });
