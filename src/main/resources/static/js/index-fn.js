dc640925a1bc640eb574e4fef786334da6f7c31e
Jihun Park <jihunpark.tech@gmail.com>
2022-12-29 22:53:19 +0900

    feat: 맞춤법 결과 체크

--- src/main/resources/static/js/index-fn.js
+++ src/main/resources/static/js/index-fn.js
@@ -6,9 +6,15 @@ function fnSend() {
         data: $("#form").serialize(),
     }).done(function(result) {
         console.log(result);
+        let originalText = result.originalText.replaceAll("\n", "<br>");
+        let correctedText = result.correctedText.replaceAll("\n", "<br>");
+        result.spellCheckErrInfo.forEach((errInfo) => {
+            originalText = originalText.replace(errInfo.orgStr, "<span style='color: red'>" + errInfo.orgStr + "</span>");
+        });
+
         $('#output').val(result.translatedText);
-        $('#original-text').val(result.originalText);
-        $('#corrected-original-text').val(result.correctedText);
+        $('#original-text').html(originalText);
+        $('#corrected-text').html(correctedText);
     }).fail(function (error) {
         $('#output').val("에러가 발생하였습니다. 잠시 후 다시 시도해 주세요.");
     });
